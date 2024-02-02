465172b2067816e55f55fe11bbace35acf91f660
JJJSCodes <meijason15@gmail.com>
2024-02-02 15:54:37 -0800

    Added backend auth to verify user. Pass in authorization code from firebase on client side as a header 'authorization'. Backend will give you uid of user

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,10 +1,36 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Profile() {
 	const { currentUser, signOut } = useContext(AuthContext);
 	const navigate = useNavigate();
+	const [loggedInState, setLoggedInState] = useState();
+
+	useEffect(() => {
+		currentUser
+			?.getIdToken()
+			.then((token) => {
+				console.log(token);
+				axios
+					.get(import.meta.env.VITE_BACKEND_URL + "/profile", {
+						headers: {
+							authorization: token,
+						},
+					})
+					.then((res) => {
+						setLoggedInState(res);
+						console.log(res);
+					})
+					.catch((err) => {
+						console.log("CORS error: ", err);
+					});
+			})
+			.catch((err) => {
+				console.log("Token err: ", err);
+			});
+	}, []);
 
 	return (
 		<div>
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,26 @@ import express, { Express, Request, Response } from "express";
 import pg from "pg";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import { initializeApp } from "firebase-admin/app";
+import isLoggedIn from "./isLoggedIn";
+import cors from "cors";
 
 dotenv.config();
 
+const firebaseConfig = {
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
+	measurementId: process.env.MEASUREMENT_ID,
+};
+
+initializeApp(firebaseConfig);
 const app: Express = express();
 const port = process.env.PORT || 3000;
 const { Client } = pg;
-
 const client = new Client({
 	user: "postgres",
 	host: "db",
@@ -16,32 +29,55 @@ const client = new Client({
 	password: "1234",
 	port: 5432,
 });
-client.connect();
-
 const createTable = async () => {
 	await client.query(`CREATE TABLE IF NOT EXISTS users 
   (id serial PRIMARY KEY, name VARCHAR (255) UNIQUE NOT NULL, 
   email VARCHAR (255) UNIQUE NOT NULL, age INT NOT NULL);`);
 };
+const allowedOrigins = ["http://localhost:5173"];
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (!origin) return callback(null, true);
+		if (allowedOrigins.indexOf(origin) === -1) {
+			var msg =
+				"The CORS policy for this site does not " +
+				"allow access from the specified Origin.";
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	},
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-createTable();
+app.use(cors(corsOptions));
+
+client
+	.connect()
+	.then(() => {
+		createTable();
+	})
+	.catch((err) => console.log(err));
 
 app.get("/api", (req: Request, res: Response) => {
-	res.send("Express + TypeScript Server");
+	res.send("Express + TypeScript Blah");
 });
 
-app.get("/api/all", async (req, res) => {
-	try {
-		const response = await client.query(`SELECT * FROM users`);
-
-		if (response) {
-			res.status(200).send(response.rows);
-		}
-	} catch (error) {
-		res.status(500).send("Error");
-		console.log(error);
-	}
-});
+app.get("/api/profile", [
+	isLoggedIn,
+	async (req, res) => {
+		const { uid, email } = req.user;
+		// try {
+		// 	const response = await client.query(`SELECT * FROM users`);
+		// 	if (response) {
+		// 		res.status(200).send(response.rows);
+		// 	}
+		// } catch (error) {
+		// 	res.status(500).send("Error");
+		// 	console.log(error);
+		// }
+		res.status(201).json({ message: "Private route", uid, email });
+	},
+]);
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
