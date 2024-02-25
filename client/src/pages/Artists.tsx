933c41ae29ff87ae44d98c2d0368d7671f22f1f7
Hong <hongvo@stanford.edu>
2024-02-24 21:57:25 -0800

    Create new gig page, clean up components for Artists and Gigs page

--- client/src/pages/Artists.tsx
+++ client/src/pages/Artists.tsx
@@ -12,127 +12,54 @@ import {
   } from "@/components/ui/card";
 import { PiMagnifyingGlassBold } from "react-icons/pi";
 import { FilterSidebarArtist } from "../components/Filterbar";
+import GigCard from "@/components/GigCards";
+
+const artists = [
+    {
+        imageUrl: "https://web.stanford.edu/group/calypso/cgi-bin/images/feelin_it.jpg",
+        title: "Cardinal Calypso",
+        bio: "Cardinal calypso is Stanford University's steelpan ensemble. Founded in 2005, it may be seen anywhere around the greater Stanford campus, causing wanton happiness and general excitement.",
+        tags: ["Steelpan", "Jazz"],
+		buttonText: "View Profile",
+    },
+    {
+        imageUrl: "https://history.stanford.edu/sites/history/files/styles/custm_medium_scale_360px_upscaling_allowed_/public/media/image/news/hume_humanities.jpg?itok=5v0SGA_H",
+        title: "Ravi Veriah Jacques",
+        bio: "Beyond the academic world, Ravi is a talented violinist; he studies with Robin Sharp in the Department of Music and received a Friends of Music scholarship to support those lessons. In 2017, he also won a Stanford Humanities and Sciences Undergraduate Prize in Music.",
+        tags: ["Violin", "Classical"],
+		buttonText: "View Profile",
+    },
+    {
+        imageUrl: "https://stanforddaily.com/wp-content/uploads/2015/03/AL.030615.SSO_.jpg?w=800",
+        title: "Jennie Yang",
+        bio: "Jennie has immersed herself in a variety of academic and artistic pursuits, often operating in the intersections between those interests. She is a longtime violist in the Stanford Symphony Orchestra, as well as a dancer in the Opening Committee of the 2020 Viennese Ball.",
+        tags: ["Violin", "Classical"],
+		buttonText: "View Profile",
+    },
+];
 
 function Artists() {
- 
-  return (
-	  <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-		<h1 style={{ textAlign: 'center' }}>Discover Artists</h1>
-  
-		{/* Search Bar and Content Container */}
-		<div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-		  {/* Left Side Content */}
-		  <div style={{ width: '66%', display: 'flex', flexDirection: 'column' }}>
-			{/* Search Bar */}
-			<div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
-			  <Input
-				placeholder="Search"
-				style={{ marginRight: '8px' }} // Add some space between the input and the button
-			  />
-			  <Button type="submit">
-				<PiMagnifyingGlassBold />
-			  </Button>
-			</div>
-  
-			
-  
-			{/* Event Container */}
-			<div>
-			  <Card style={{
-				border: '1px solid #ccc', 
-				boxShadow: '0 2px 4px rgba(0,0,0,0.1)', 
-				display: 'flex', 
-				flexDirection: 'row', 
-				alignItems: 'flex-start' 
-			  }}>
-				{/* Image Container */}
-				<div style={{ minWidth: '200px', maxWidth: '30%', overflow: 'hidden' }}>
-				  <img src="https://web.stanford.edu/group/calypso/cgi-bin/images/feelin_it.jpg" alt="Event" style={{ width: '100%', height: 'auto' }} />
-				</div>
-  
-				{/* Text Content Container */}
-				<div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-				  <CardHeader>
-					<CardTitle>Cardinal Calypso</CardTitle>
-				  </CardHeader>
-				  <CardContent>
-					<CardDescription>Cardinal calypso is stanford university's steelpan ensemble. Founded in 2005, it may be seen anywhere around the greater stanford campus, causing wanton happiness and general excitement. </CardDescription>
-				  </CardContent>
-				  <CardFooter>
-					<Button variant="secondary">Steelpan </Button> <Button variant="secondary">Jazz</Button> 
-				  </CardFooter>
-				</div>
-			  </Card>
-			</div>
-
-			{/* second event */}
-
-			<div>
-			  <Card style={{
-				border: '1px solid #ccc', 
-				boxShadow: '0 2px 4px rgba(0,0,0,0.1)', 
-				display: 'flex', 
-				flexDirection: 'row', 
-				alignItems: 'flex-start' 
-			  }}>
-				{/* Image Container */}
-				<div style={{ minWidth: '200px', maxWidth: '30%', overflow: 'hidden' }}>
-				  <img src="https://history.stanford.edu/sites/history/files/styles/custm_medium_scale_360px_upscaling_allowed_/public/media/image/news/hume_humanities.jpg?itok=5v0SGA_H" alt="Event" style={{ width: '100%', height: 'auto' }} />
-				</div>
-  
-				{/* Text Content Container */}
-				<div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-				  <CardHeader>
-					<CardTitle>Ravi Veriah Jacques </CardTitle>
-				  </CardHeader>
-				  <CardContent>
-					<CardDescription> Beyond the academic world, Ravi is a talented violinist; he studies with Robin Sharp in the Department of Music and received a Friends of Music scholarship to support those lessons. In 2017, He also won a Stanford Humanities and Sciences Undergraduate Prize in Music.   </CardDescription>
-				  </CardContent>
-				  <CardFooter>
-					<Button variant="secondary">Violin</Button>  <Button variant="secondary">Classical</Button> 
-				  </CardFooter>
-				</div>
-			  </Card>
-			</div>
-
-      {/* third event */}
-      <div>
-			  <Card style={{
-				border: '1px solid #ccc', 
-				boxShadow: '0 2px 4px rgba(0,0,0,0.1)', 
-				display: 'flex', 
-				flexDirection: 'row', 
-				alignItems: 'flex-start' 
-			  }}>
-				{/* Image Container */}
-				<div style={{ minWidth: '200px', maxWidth: '30%', overflow: 'hidden' }}>
-				  <img src="https://stanforddaily.com/wp-content/uploads/2015/03/AL.030615.SSO_.jpg?w=800" alt="Event" style={{ width: '100%', height: 'auto' }} />
-				</div>
-  
-				{/* Text Content Container */}
-				<div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-				  <CardHeader>
-					<CardTitle> Jennie Yang </CardTitle>
-				  </CardHeader>
-				  <CardContent>
-					<CardDescription>Jennie has immersed herself in a variety of academic and artistic pursuits, often operating in the intersections between those interests. She is a longtime violist in the Stanford Symphony Orchestra, as well as a dancer in the Opening Committee of the 2020 Viennese Ball.      </CardDescription>
-				  </CardContent>
-				  <CardFooter>
-					<Button variant="secondary">Violin</Button>  <Button variant="secondary">Classical</Button> 
-				  </CardFooter>
-				</div>
-			  </Card>
-			</div>
-			
-		  </div>
-
-		  
-      <FilterSidebarArtist />
-
-		</div>
-
-	  </div>
-	);
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+            <h1 style={{ textAlign: 'center' }}>Discover Artists</h1>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+                <div style={{ width: '66%', display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                        <Input placeholder="Search" style={{ marginRight: '8px' }} />
+                        <Button type="submit">
+                            <PiMagnifyingGlassBold />
+                        </Button>
+                    </div>
+                    {artists.map((artist, index) => (
+                        <GigCard key={index} imageUrl={artist.imageUrl} title={artist.title} bio={artist.bio} tags={artist.tags} buttonText={artist.buttonText}
+						onButtonClick={() => console.log('Button Clicked')}
+						/>
+                    ))}
+                </div>
+                <FilterSidebarArtist />
+            </div>
+        </div>
+    );
 }
 
 export default Artists;
--- client/src/pages/Gigs.tsx
+++ client/src/pages/Gigs.tsx
@@ -1,6 +1,6 @@
 
 import React, { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { Toaster } from "@/components/ui/sonner";
 import { Button } from "@/components/ui/button";
@@ -15,127 +15,80 @@ import {
   } from "@/components/ui/card";
   import FilterSidebarGig from "@/components/Filterbar";
 import { PiMagnifyingGlassBold } from "react-icons/pi";
-import { IoMdAddCircle } from "react-icons/io";
+import GigCard from "@/components/GigCards"; 
+import Gig1 from "../assets/gigs/add.png";
 
+
+
+const gigs = [
+    {
+        imageUrl: "https://stanforddaily.com/wp-content/uploads/2022/05/IMG_5407.jpg",
+        title: "Frosh Fest 2024",
+        bio: "This year’s event will host over 2000 attendees, well over 50 breweries from across the region, food trucks, bands, and vendors for what will be a true celebration of craft beer, art, and community. It is important to note that Frost Fest will take place regardless of the weather.",
+        tags: ["Jazz", "Rock", "Pop"],
+        buttonText: "Learn More" 
+    },
+    {
+        imageUrl: "https://www.sfcv.org/sites/default/files/u36849/xCatalina.png.pagespeed.ic.C4GpEqvNmv.jpg",
+        title: "Stanford Classical Ensembles",
+        bio: "The Department of Music hosts many performing ensembles and combos. Some ensembles are open to community members. Be prepared to play études, a sonata, a concerto excerpt, or anything else that demonstrates your abilities.",
+        tags: ["Jazz", "Classical"],
+        buttonText:  "Learn More" 
+    },
+    // Add more gigs as necessary
+];
+
+const navigate = useNavigate(); // This hook gives you access to the navigate function
+
+const handleCreateGig = () => {
+	navigate('/create-gig'); // Use the path to your CreateGig page
+};
 
 function EventsList() {
-	return (
-	  <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-		<h1 style={{ textAlign: 'center' }}>Discover Gigs</h1>
-  
-		{/* Search Bar and Content Container */}
-		<div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-		  {/* Left Side Content */}
-		  <div style={{ width: '66%', display: 'flex', flexDirection: 'column' }}>
-			{/* Search Bar */}
-			<div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
-			  <Input
-				placeholder="Search"
-				style={{ marginRight: '8px' }} // Add some space between the input and the button
-			  />
-			  <Button type="submit">
-				<PiMagnifyingGlassBold />
-			  </Button>
-			</div>
-  
-			{/* New Event */}
-			<div style={{ marginBottom: '20px' }}>
-			  <Card style={{ border: '1px solid #ccc', boxShadow: '0 2px 4px rgba(0,0,0,0.1)' }}>
-				<CardHeader>
-				  <CardTitle>Create new gig</CardTitle>
-				</CardHeader>
-				<CardContent>
-				  <CardDescription> Set up your own gig here now! </CardDescription>
-				</CardContent>
-				<CardFooter>
-				  <Button
-					variant="destructive"
-					onClick={() =>
-					  toast.success("Gig has been created", {
-						description: "Sunday, June 03, 2024 at 9:00 PM",
-						action: {
-						  label: "Undo",
-						  onClick: () => console.log("Undo"),
-						},
-					  })
-					}
-				  >
-					CREATE <IoMdAddCircle />
-				  </Button>
-				</CardFooter>
-				<Toaster position="top-center" richColors/>
-			  </Card>
-			</div>
-  
-			{/* Event Container */}
-			<div>
-			  <Card style={{
-				border: '1px solid #ccc', 
-				boxShadow: '0 2px 4px rgba(0,0,0,0.1)', 
-				display: 'flex', 
-				flexDirection: 'row', 
-				alignItems: 'flex-start' 
-			  }}>
-				{/* Image Container */}
-				<div style={{ minWidth: '200px', maxWidth: '30%', overflow: 'hidden' }}>
-				  <img src="https://stanforddaily.com/wp-content/uploads/2022/05/IMG_5407.jpg" alt="Event" style={{ width: '100%', height: 'auto' }} />
-				</div>
-  
-				{/* Text Content Container */}
-				<div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-				  <CardHeader>
-					<CardTitle>Frosh Fest 2024</CardTitle>
-				  </CardHeader>
-				  <CardContent>
-					<CardDescription> This year’s event will host over 2000 attendees, well over 50 breweries from across the region, food trucks, bands and vendors for what will be a true celebration of craft beer, art, and community. It is important to note that Frost Fest will take place regardless of the weather. </CardDescription>
-				  </CardContent>
-				  <CardFooter>
-					<Button variant="secondary">Jazz</Button> <Button variant="secondary">Rock</Button> <Button variant="secondary">Pop</Button>
-				  </CardFooter>
-				</div>
-			  </Card>
-			</div>
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+            <h1 style={{ textAlign: 'center' }}>Discover Gigs</h1>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+                <div style={{ width: '66%', display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                        <Input placeholder="Search" style={{ marginRight: '8px' }} />
+                        <Button type="submit">
+                            <PiMagnifyingGlassBold />
+                        </Button>
+                    </div>
 
-			{/* second event */}
+					{/* Create New Gig Card */}
+                    <GigCard 
+                        imageUrl= {Gig1} 
+                        title="Create New Gig"
+                        bio="Set up your own gig here now!"
+                        tags={["Create", "New"]} // Placeholder tags
+                        buttonText="Create"
+						onButtonClick={handleCreateGig}
+                    />
 
-			<div>
-			  <Card style={{
-				border: '1px solid #ccc', 
-				boxShadow: '0 2px 4px rgba(0,0,0,0.1)', 
-				display: 'flex', 
-				flexDirection: 'row', 
-				alignItems: 'flex-start' 
-			  }}>
-				{/* Image Container */}
-				<div style={{ minWidth: '200px', maxWidth: '30%', overflow: 'hidden' }}>
-				  <img src="https://www.sfcv.org/sites/default/files/u36849/xCatalina.png.pagespeed.ic.C4GpEqvNmv.jpg" alt="Event" style={{ width: '100%', height: 'auto' }} />
-				</div>
-  
-				{/* Text Content Container */}
-				<div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-				  <CardHeader>
-					<CardTitle>Stanford Classical Ensembles</CardTitle>
-				  </CardHeader>
-				  <CardContent>
-					<CardDescription> The Department of Music hosts many performing ensembles and combos. Some ensembles are open to community members. Be prepared to play études, a sonata, a concerto excerpt, or anything else that demonstrates your abilities.  </CardDescription>
-				  </CardContent>
-				  <CardFooter>
-					<Button variant="secondary">Jazz</Button>  <Button variant="secondary">Classical</Button> 
-				  </CardFooter>
-				</div>
-			  </Card>
-			</div>
-			
-		  </div>
 
-		  {/* Filter Sidebar on the Right */}
-		  <FilterSidebarGig />
-		</div>
-	  </div>
-	);
-  }
+                    {gigs.map((gig, index) => (
+                        <GigCard 
+                            key={index} 
+                            imageUrl={gig.imageUrl} 
+                            title={gig.title} 
+                            bio={gig.bio} 
+                            tags={gig.tags} 
+                            buttonText={gig.buttonText} 
+							onButtonClick={() => console.log('Button Clicked')}
+                        />
+                    ))}
+                </div>
+                <FilterSidebarGig />
+            </div>
+            <Toaster position="top-center" richColors/>
+        </div>
+    );
+}
 
-  
 export default EventsList;
+  
+
 
 
