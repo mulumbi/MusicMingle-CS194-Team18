8a1527f16524e508431229fbd3d17c6fa91e30c3
JJJSCodes <meijason15@gmail.com>
2024-03-04 14:11:03 -0800

    Search now works for artists

--- client/src/api/artists.api.ts
+++ client/src/api/artists.api.ts
@@ -1,21 +1,34 @@
-
 import axios from "axios";
 import { Artist } from "./types";
 
 export const fetchArtists = async (searchParams: {
-    name?: string;
-    user_role_tags?: string[];
+	name?: string;
+	user_role_tags?: string[];
 }): Promise<Artist[]> => {
-    const query = new URLSearchParams();
-    if (searchParams.name) query.append('name', searchParams.name);
-    if (searchParams.user_role_tags) query.append('user_role_tags', JSON.stringify(searchParams.user_role_tags));
-
-    const data = await axios
-        .get(`${import.meta.env.VITE_BACKEND_URL}/artists/?${query.toString()}`)
-        .then((res) => res.data)
-        .catch((err) => {
-            console.error("Error fetching artists: ", err);
-            throw err;
-        });
-    return data;
+	console.log(searchParams, "searchParams");
+	const query = new URLSearchParams();
+	if (searchParams.name) query.append("name", searchParams.name);
+	if (searchParams.user_role_tags)
+		query.append(
+			"user_role_tags",
+			JSON.stringify(searchParams.user_role_tags)
+		);
+	console.log(query, query.toString(), "query");
+	const data = await axios
+		.get(
+			import.meta.env.VITE_BACKEND_URL +
+				"/artists" +
+				"?" +
+				query.toString()
+		)
+		.then((res) => {
+			console.log(res);
+			const artists: [Artist] = res.data;
+			return artists;
+		})
+		.catch((err) => {
+			console.error("Error fetching artists: ", err);
+			throw err;
+		});
+	return data;
 };
--- client/src/pages/Artists.tsx
+++ client/src/pages/Artists.tsx
@@ -1,6 +1,5 @@
-import React, { useContext, useState } from "react";
-import { useQuery } from '@tanstack/react-query';
-import { AuthContext } from "../context/AuthContext";
+import React, { useContext, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -10,12 +9,11 @@ import {
 	CardFooter,
 	CardHeader,
 	CardTitle,
-  } from "@/components/ui/card";
+} from "@/components/ui/card";
 import { PiMagnifyingGlassBold } from "react-icons/pi";
 import { FilterSidebarArtist } from "../components/Filterbar";
 import GigCard from "@/components/GigCards";
 import { fetchArtists } from "../api/artists.api";
-
 
 // const artists = [
 //     {
@@ -44,56 +42,78 @@ import { fetchArtists } from "../api/artists.api";
 //     },
 // ];
 
-
 function Artists() {
+	const [searchName, setSearchName] = useState("");
+	const [roleTags, setRoleTags] = useState<string[]>([]);
 
-    const [searchName, setSearchName] = useState('');
-    const [roleTags, setRoleTags] = useState<string[]>([]);
+	const { data, error, isLoading, refetch } = useQuery({
+		queryKey: ["artists"],
+		queryFn: () =>
+			fetchArtists({ name: searchName, user_role_tags: roleTags }),
+		enabled: false,
+	});
 
-    const { data: artists } = useQuery({
-        queryKey: ['artists', { name: searchName, user_role_tags: roleTags }],
-        queryFn: () => fetchArtists({ name: searchName, user_role_tags: roleTags }),
-        // keepPreviousData: true,
-    });
-    return (
-        <div className="App" id="ArtistPageApp" >
-            <div>
-            <div className="Title" style={{margin: "20px"}}>Discover Artists</div>
-            <div id="ArtistBody" >
-                <div className="Artist-page-cards">
-                    <div style = {{textAlign: "center"}}>
-                        <Input placeholder="Search" 
-                        value={searchName}
-                        onChange={(e) => setSearchName(e.target.value)}/>
-                        <Button type="submit" >
-                            <PiMagnifyingGlassBold />
-                        </Button>
-                    </div>
+	useEffect(() => {
+		refetch();
+	}, []);
 
+	const handleSubmit = () => {
+		console.log("refetch");
+		refetch();
+	};
+	console.log(data);
+	return (
+		<div
+			className="App"
+			id="ArtistPageApp"
+		>
+			<div>
+				<div
+					className="Title"
+					style={{ margin: "20px" }}
+				>
+					Discover Artists
+				</div>
+				<div id="ArtistBody">
+					<div className="Artist-page-cards">
+						<div style={{ textAlign: "center" }}>
+							<Input
+								placeholder="Search"
+								value={searchName}
+								onChange={(e) => setSearchName(e.target.value)}
+							/>
+							<Button
+								type="submit"
+								onClick={() => handleSubmit()}
+							>
+								<PiMagnifyingGlassBold />
+							</Button>
+						</div>
 
-                        
-                    {(artists?.map((artist, index) => (
-                                <GigCard 
-                                    key={index} 
-                                    // imageUrl={artist.profile_image}
-                                    imageUrl={artist.profile_image?.public_url || ''}
-                                    title={artist.name} 
-                                    bio={artist.bio} 
-                                    tags={artist.user_genre_tags.concat(artist.user_role_tags)} 
-                                    buttonText="View Profile"
-                                    onButtonClick={() => console.log('Artist Clicked', artist.name)}
-                                />
-                            ))
-                        )}
-                </div>
-               
-            </div>
-            </div>
-            <FilterSidebarArtist rate={5} />   
-            
-        </div>
-    );
+						{data?.map((artist, index) => (
+							<GigCard
+								key={index}
+								// imageUrl={artist.profile_image}
+								imageUrl={
+									artist.profile_image?.public_url || ""
+								}
+								title={artist.name}
+								bio={artist.bio}
+								tags={artist.user_genre_tags.concat(
+									artist.user_role_tags
+								)}
+								buttonText="View Profile"
+								onButtonClick={() =>
+									console.log("Artist Clicked", artist.name)
+								}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+			<FilterSidebarArtist rate={5} />
+		</div>
+	);
 }
-
 
 export default Artists;
