d4fb23ff8bb9c8f2c19060a2c1bfa92f54867d4d
angelsc5 <97258110+angelsc5@users.noreply.github.com>
2024-03-07 16:10:00 -0800

    Update mygigs.api.ts with mutate for close gig and withdraw app

--- client/src/api/mygigs.api.ts
+++ client/src/api/mygigs.api.ts
@@ -20,3 +20,46 @@ export const fetchGigsData = async (currentUser: any): Promise<any> => {
 		});
 	return response;
 };
+
+// For closing a gig
+export const mutateCloseGig = async (
+	currentUser: any,
+	gig_id: any
+) => {
+	const token = await currentUser.getIdToken();
+	const query = new URLSearchParams();
+	query.append("gig_id", gig_id)
+	
+	const response = await axios
+		.post(`${import.meta.env.VITE_BACKEND_URL}/mygigs/close_gig?` + query.to_string(), {
+			headers: { authorization: token },
+		})
+		.then((res) => {
+			const gig: Gig = res.data;
+			console.log(gig, "Successfully closed gig!");
+			return gig;
+		})
+		.catch((err) => console.error("my Gigs close gig mutate error: ", err));
+	return response;
+};
+
+// For withdrawing an application
+export const mutateWithdrawApp = async (
+	currentUser: any,
+	gig_id: any
+) => {
+	const token = await currentUser.getIdToken();
+	const query = new URLSearchParams();
+	query.append("gig_id", gig_id)
+	
+	const response = await axios
+		.post(`${import.meta.env.VITE_BACKEND_URL}/mygigs/withdraw_app?` + query.to_string(), {
+			headers: { authorization: token },
+		})
+		.then((res) => {
+			console.log(res, "res");
+			return res.data;
+		})
+		.catch((err) => console.error("my Gigs withdraw app mutate error: ", err));
+	return response;
+};
