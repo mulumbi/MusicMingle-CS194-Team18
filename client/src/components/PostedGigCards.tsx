44a0131ab0cf5051817008495f121c86e41e33ab
angelsc5 <97258110+angelsc5@users.noreply.github.com>
2024-03-08 05:24:12 -0800

    updated PostedGigCard to have a popup for viewing applicants

--- client/src/components/PostedGigCards.tsx
+++ client/src/components/PostedGigCards.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./GigCard.css";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/ui/popover";
 
 interface PostedGigCardProps {
         title: string;
@@ -7,12 +12,12 @@ interface PostedGigCardProps {
       	eventStart: string; // Changed to string to match the expected type from API
       	eventEnd: string; // Changed to string to match the expected type from API
       	tags: string[];
+        popupButtonText: string;
+        popupContent: any;
       	buttonText1: string;
       	onButtonClick1: () => void;
         buttonText2: string;
       	onButtonClick2: () => void;
-        buttonText3: string;
-      	onButtonClick3: () => void;
 }
 
 function formatDateTime(dateString: string): string {
@@ -41,43 +46,50 @@ const PostedGigCard: React.FC<PostedGigCardProps> = ({
       	eventStart,
       	eventEnd,
       	tags,
+        popupButtonText,
+        popupContent,
       	buttonText1,
       	onButtonClick1,
         buttonText2,
       	onButtonClick2,
-        buttonText3,
-      	onButtonClick3,
 }) => (
       	<div className="my-gig-cards">
-            		<img
-                  			src={imageUrl}
-                  			alt="Gig"
-            		/>
-            		<div className="gig-details">
-                  			<div className="gig-header">{title}</div>
-                  			<p className="gig-bio">{bio}</p>
-                  			<p>
-                  				      {formatDateTime(eventStart)} to {formatDateTime(eventEnd)}
-                  			</p>
-                  			<div className="gig-bottoms">
-                        				<div className="gig-tabs">
-                              					{/* Dynamically generate spans for each tag */}
-                              					{tags?.map((tag, index) => (
-                                    						<span
-                                    							      key={index}
-                                    							      className="tab"
-                                    						>
-                                    							      {tag}
-                                    						</span>
-                              					))}
-                        				</div>
-                        				<div className="bottom-buttons">
-                        					      <button onClick={onButtonClick1}>{buttonText1}</button>
+                <img
+                        src={imageUrl}
+                        alt="Gig"
+                />
+                <div className="gig-details">
+                        <div className="gig-header">{title}</div>
+                        <p className="gig-bio">{bio}</p>
+                        <p>
+                              {formatDateTime(eventStart)} to {formatDateTime(eventEnd)}
+                        </p>
+                        <div className="gig-bottoms">
+                                <div className="gig-tabs">
+                                        {/* Dynamically generate spans for each tag */}
+                                        {tags?.map((tag, index) => (
+                                                <span
+                                                        key={index}
+                                                        className="tab"
+                                                >
+                                                        {tag}
+                                                </span>
+                                        ))}
+                                </div>
+                                <div className="bottom-buttons">
+                                        <Popover>
+                                                <PopoverTrigger asChild>
+                                                        <button>{popupButtonText}</button>
+                                                </PopoverTrigger>
+                                                <PopoverContent>
+                                                        {popupContent}
+                                                </PopoverContent>
+                                        </Popover>
+                                        <button onClick={onButtonClick1}>{buttonText1}</button>
                                         <button onClick={onButtonClick2}>{buttonText2}</button>
-                                        <button onClick={onButtonClick3}>{buttonText3}</button>
-                        				</div>
-                  			</div>
-            		</div>
+                                </div>
+                        </div>
+                </div>
       	</div>
 );
 
