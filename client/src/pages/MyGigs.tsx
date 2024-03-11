f513ca694e6a6b05a90dde39429c06c05eb0f843
angelsc5 <97258110+angelsc5@users.noreply.github.com>
2024-03-10 22:33:35 -0700

    debug imports

--- client/src/pages/MyGigs.tsx
+++ client/src/pages/MyGigs.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
